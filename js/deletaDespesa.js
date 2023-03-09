250b76b83bb2719c40bf1835d8b6ca8a309457fd
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-03-09 14:42:04 -0300

    .

--- js/deletaDespesa.js
+++ js/deletaDespesa.js
@@ -1,11 +1,11 @@
 
 
-function deletaElemento(tag, id){
+function deletaElemento(tag, id, valorRetirado){
 	tag.remove();
 
 	despesas.splice(despesas.findIndex(elemento => elemento.id === id), 1);
 
-	/*total = parseFloat(total) - parseFloat(valorRetirado);
+	total = parseFloat(total) - parseFloat(valorRetirado);
 	document.getElementById("calc-total").innerHTML = total.toFixed(2);
-	localStorage.setItem("total", JSON.stringify(total.toFixed(2)));*/
+	localStorage.setItem("total", JSON.stringify(total.toFixed(2)));
 }
--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ form.addEventListener("submit", function(evento){
 lista.addEventListener("dblclick", function(evento, despesa){
 	let alvo = event.target;
 
-	alvo.deletaElemento(this.parentNode, despesa.id);
+	alvo.deletaElemento(this.parentNode, despesa.id.value, despesa.valor);
 });
 
 
